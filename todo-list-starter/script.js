2decf030d372592a65294a5284d2853b354396a1
Caroline99 <carolinejbirch@gmail.com>
2022-07-13 22:29:14 +1000

    Completed Todo List

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -1,7 +1,82 @@
-function addTask() {}
+// let todoTasksText = ["Walk Chilli", "Make Dinner"];
+let todoTasksText = []
+// let todoTasksStatus = [false, true];
+let todoTasksStatus = []
 
-function updateTodoList() {}
+let todoList = document.getElementById("todo-list");
+updateTodoList();
 
-function createNewTodoItemElement(todoItem, index) {}
+// todoTasksText.forEach((task, index) => {
+//     let newTodoTaskTextElement = document.createElement("p");
+//     newTodoTaskTextElement.innerText = task;
 
-function toggleComplete(index) {}
+//     if (todoTasksStatus[index] == true) {
+//         newTodoTaskTextElement.classList.add("complete");
+//     }
+
+//     let newTodoTaskElement = document.createElement("li");
+//     newTodoTaskElement.appendChild(newTodoTaskTextElement);
+//     let completeButtonElement = document.createElement("input");
+//     completeButtonElement.type = "button";
+//     completeButtonElement.value = "Completed";
+//     completeButtonElement.onclick = function() {
+//         toggleComplete(index);
+//     }
+//     newTodoTaskElement.appendChild(completeButtonElement);
+//     todoList.appendChild(newTodoTaskElement);
+// });
+
+function addTask() {
+    let newTask = document.getElementById("new-task-text");
+    if (newTask.value) {
+        todoTasksText.push(newTask.value);
+        todoTasksStatus.push(false);
+        newTask.value = "";
+        console.log(todoTasksText);
+        updateTodoList();
+    }
+}
+
+function updateTodoList() { 
+    let todoList = document.getElementById("todo-list");
+    todoList.innerHTML = "";
+    todoTasksText.forEach((task, index) => {
+        let newTodoTaskElement = createNewTodoTaskElement(task, index);
+        todoList.appendChild(newTodoTaskElement);
+    })
+}
+
+function createNewTodoTaskElement(task, index) {
+    let newTodoTaskTextElement = document.createElement("p");
+    newTodoTaskTextElement.innerText = task;
+
+    if (todoTasksStatus[index] == true) {
+        newTodoTaskTextElement.classList.add("complete");
+    }
+
+    let newTodoTaskElement = document.createElement("li");
+    newTodoTaskElement.appendChild(newTodoTaskTextElement);
+    let completeButtonElement = document.createElement("input");
+    completeButtonElement.type = "button";
+    completeButtonElement.value = "Completed";
+    completeButtonElement.onclick = function() {
+        toggleComplete(index);
+    }
+    newTodoTaskElement.appendChild(completeButtonElement);
+    todoList.appendChild(newTodoTaskElement);
+    return newTodoTaskElement;
+}
+
+// todoTasksText.forEach((task, index) =>  {
+//     createNewTodoItemElement(task, index);
+// })
+
+function toggleComplete(index) {
+    if (todoTasksStatus[index] == false) {
+        todoTasksStatus[index] = true;
+    } else {
+        todoTasksStatus[index] = false;
+    }
+    console.log(todoTasksStatus);
+    updateTodoList();
+}
