919a65371276b94a288e8b3c1341227781380f61
Caroline99 <carolinejbirch@gmail.com>
2022-07-13 22:28:32 +1000

    explanation of js creating html

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,11 +1,43 @@
-function showName() {
-    // console.log("Hello, world!");
-    let inputField = document.getElementById('name-text')
-    // alert("Hello Caroline")
-    // console.log("The Input Field Is: ", inputField)
-    // console.dir(inputField)
-    alert("Hello " + inputField.value)
-}
+// window.onload = function () {
+//     let speechBubbleElement = document.createElement("div");
+//     // <div></div>
+
+//     speechBubbleElement.classList.add('speech-bubble')
+//     // <div class="speech-bubble"></div>
+
+//     let messageBox = document.createElement("p");
+//     // <p></p>
+//     messageBox.innerText = "...";
+//     // <p>...</p>
+//     messageBox.id = 'message-box'
+//     // <p id="message-box">...</p>
+
+//     speechBubbleElement.appendChild(messageBox);
+//     /*
+//       <div class="speech-bubble">
+//         <p id="message-box">...</p>
+//       </div>
+//     */
+
+//     let outputElement = document.getElementById("output");
+//     outputElement.appendChild(speechBubbleElement);
+//     /*
+//       <div id="output">
+//         <div class="speech-bubble">
+//           <p id="message-box">...</p>
+//         </div>
+//       </div>
+//     */
+// };
+
+// function showName() {
+//     // console.log("Hello, world!");
+//     let inputField = document.getElementById('name-text')
+//     // alert("Hello Caroline")
+//     // console.log("The Input Field Is: ", inputField)
+//     // console.dir(inputField)
+//     alert("Hello " + inputField.value)
+// }
 
 
 // for (let count = 0; count < 10; count++) {
@@ -26,3 +58,14 @@ function showName() {
 // pets.forEach((pet) => {
 //     console.log(pet)
 // })
+
+// pet = ["Ivy"]
+
+// if (pet == "Chilli") {
+//     console.log("OMG you are the cutest!");
+// } else if (pet == "Ivy") {
+//     console.log("Naww cute bow. ")
+// } else {
+//     console.log("OMG so cute!");
+// }
+
