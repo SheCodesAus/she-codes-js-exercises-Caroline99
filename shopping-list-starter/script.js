0caca3ae7b9372178f6c06740ff98b9789d362a5
Caroline99 <carolinejbirch@gmail.com>
2022-07-13 19:06:47 +1000

    Jumping ahead

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,38 @@
-function addItem() {}
+let shoppingListItems = ["milk", "eggs", "bread"]
+let listElement = document.getElementById("shopping-list-items")
+    // listElement.innerHTML = ""
+shoppingListItems.forEach((item) => {
+    // console.log(item)
+    let itemElement = document.createElement("li")
+    itemElement.innerText = item
+    listElement.appendChild(itemElement)
+})
 
-function updateItems() {}
+
+function addItem() {
+    let item = document.getElementById("new-item-text").value 
+    shoppingListItems.push(item)
+    console.log(shoppingListItems)
+    updateItems()
+}
+
+function updateItems() {
+    
+    let listElement = document.getElementById("shopping-list-items")
+    listElement.innerHTML = ""
+    shoppingListItems.forEach((item) => {
+        console.log(item)
+        let itemElement = document.createElement("li")
+        itemElement.innerText = item
+        listElement.appendChild(itemElement)
+    })
+}
+
+function clearItems() {
+    let listElement = document.getElementById("shopping-list-items")
+    listElement.innerHTML = ""
+    shoppingListItems = []
+    console.log(shoppingListItems)
+}
+
+
